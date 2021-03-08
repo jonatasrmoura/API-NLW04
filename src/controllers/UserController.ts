a4b2e08c746f8fca64ceb8ec38af20002b080355
jonatasrmoura <jonatas.rmoura@outlook.com>
2021-03-07 21:46:43 -0300

    feat(criaçao de validaçao de usuario)

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,10 +1,25 @@
 import { Request, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
 import { UsersRepository } from '../repositories/UsersRepository';
-
+import * as yup from 'yup';
 export class UsersController {
     async create(request: Request, response: Response) {
         const { name, email } = request.body;
+
+        // Validando Campos de criação de usuário
+
+        const schema = yup.object().shape({
+            name: yup.string().required('Nome é obrigatório'),
+            email: yup.string().email().required('E-mail incorreto'),
+        });
+
+        try {
+            await schema.validate(request.body, { abortEarly: false }); // rodar todas as validações
+        } catch (err) {
+            return response.status(400).json({ error: err });
+        }
+
+        // Entidade do usuário
 
         const usersRepository = getCustomRepository(UsersRepository);
 
